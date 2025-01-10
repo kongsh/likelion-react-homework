09054de7bbf5e2ab163a204987a708bbf95cf89d
kongsh <ksh080838@gmail.com>
2025-01-10 17:13:37 +0900

    [구성] 초기 환경 구성
    - vite + TS + React

---
+++ src/components/App.ts
@@ -0,0 +1,15 @@
+import React, { Fragment } from "react";
+import Switch from "./Switch";
+
+const { createElement: h } = React;
+
+export default function App() {
+  return h(
+    Fragment,
+    null,
+    Switch({
+      id: "switch-1",
+      labelText: "switch-1",
+    })
+  );
+}
---
+++ src/main.ts
@@ -0,0 +1,11 @@
+import React from "react";
+import App from "./components/App";
+import ReactDom from "react-dom/client";
+
+const root = document.getElementById("react");
+
+if (!root) {
+  alert("문서에 #react 요소가 존재하지 않습니다.");
+} else {
+  ReactDom.createRoot(root).render(React.createElement(App));
+}

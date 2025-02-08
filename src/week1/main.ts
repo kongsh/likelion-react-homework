422f8e5b3590ec30e960cb4e28068e334fcd8dc2
kongsh <ksh080838@gmail.com>
2025-02-08 22:34:41 +0900

    [구성] build를 위해 오류 수정
    - 1주차 과제의 App을 올바르게 불러옴

--- src/week1/main.ts
+++ src/week1/main.ts
@@ -1,5 +1,5 @@
 import React from "react";
-import App from "./components/App";
+import { App } from "./components/App";
 import ReactDom from "react-dom/client";
 
 const root = document.getElementById("react");

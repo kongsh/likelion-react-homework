386418068bbf73654b2ca60bbad7cbefea589ab4
kongsh <ksh080838@gmail.com>
2025-02-08 21:42:30 +0900

    [실습] Cart
    - 상품(PRODUCTS) 정보 작성
    - 고른 상품 갯수(counts)를 useState로 관리
    - handleIncrease에서, 해당 상품의 최대 갯수와 현재 카운트 갯수를 비교하는 조건 추가
    - handleDecrease에서는 1보다 크다면 감소할 수 있도록 조건 추가
    - 리스트 렌더링을 통해 li의 key 값을 상품의 id로 제공

--- src/week5/Cart.tsx
+++ src/week5/Cart.tsx
@@ -1,9 +1,77 @@
-import Counter from "./components/counter";
+import { useState } from "react";
+import CartFooter from "./components/cart-footer";
+import CartHeader from "./components/cart-header";
+import ProductList from "./components/product-list";
+
+const PRODUCTS = [
+  {
+    id: "item-1",
+    productName: "1A 우유 900mL",
+    price: 1880,
+    productImg: "assets/week5/milk.png",
+    maxCount: 3,
+  },
+  {
+    id: "item-2",
+    productName: "맛있는 콩나물 500g",
+    price: 1280,
+    productImg: "assets/week5/bean.png",
+    maxCount: 5,
+  },
+  {
+    id: "item-3",
+    productName: "고소한 두부 1kg",
+    price: 2280,
+    productImg: "assets/week5/tofu.png",
+    maxCount: 4,
+  },
+];
+
+const INITIAL_PRODUCTS_COUNT = [1, 1, 1];
 
 function Cart() {
+  const [counts, setCounts] = useState(INITIAL_PRODUCTS_COUNT);
+
+  const totalPriceNumber = PRODUCTS.reduce((acc, { price }, index) => {
+    const count = counts[index];
+
+    return acc + price * count;
+  }, 0);
+  const totalPrice = totalPriceNumber.toLocaleString();
+
+  const handleIncrease = (i: number) => {
+    const nextCounts = [...counts].map((item, index) => {
+      if (index === i && PRODUCTS[i].maxCount !== counts[i]) {
+        return item + 1;
+      }
+      return item;
+    });
+
+    setCounts(nextCounts);
+  };
+
+  const handleDecrease = (i: number) => {
+    const nextCounts = [...counts].map((item, index) => {
+      if (index === i && counts[i] > 1) {
+        return item - 1;
+      }
+      return item;
+    });
+
+    setCounts(nextCounts);
+  };
+
   return (
-    <div>
-      <Counter></Counter>
+    <div className="p-5">
+      <CartHeader />
+      <ul className="flex flex-col gap-5 my-5">
+        {PRODUCTS.map(({ productName, productImg, price, id }, index) => (
+          <li key={id}>
+            <ProductList productName={productName} price={price} productImg={productImg} count={counts[index]} handleIncrease={() => handleIncrease(index)} handleDecrease={() => handleDecrease(index)} />
+          </li>
+        ))}
+      </ul>
+      <CartFooter totalPrice={totalPrice} />
     </div>
   );
 }
